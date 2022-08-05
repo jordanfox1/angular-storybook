2b487f07ee809381f5965a733fcd772b97f0d6dd
Jordan Fox <jordan.fox@canstar.com.au>
2022-08-05 16:34:51 +1000

    updated storybook/main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,5 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
+  stories: ['../src/app/components/**/*.stories.ts'],
   framework: "@storybook/angular",
   core: {
     builder: "@storybook/builder-webpack5",
